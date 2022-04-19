699ce24cca8635160dfd4ca194babbe533e36e5e
ShivaniYS <87171146+ShivaniYS@users.noreply.github.com>
2022-04-19 18:41:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ prompt("what's their number");
 
 var loveScore = Math.random * 100;
 loveScore = Math.floor(loveScore) + 1;
+if (loveScore > 70) {
+  alert("Your loveScore is " + loveScore + "%" + "like kanye and kanye .");
+} else {
+  alert("Your loveScore is " + loveScore + "%" );
+}
+  
 console.log(lovescore);
